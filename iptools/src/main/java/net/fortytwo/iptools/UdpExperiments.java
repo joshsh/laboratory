b0e7c46969676f601a592a4b9c8ae65ffe2aa084
Joshua Shinavier <josh@fortytwo.net>
2010-09-10 12:08:15 +0800

    Tweak

--- iptools/src/main/java/net/fortytwo/iptools/UdpExperiments.java
+++ iptools/src/main/java/net/fortytwo/iptools/UdpExperiments.java
@@ -1,56 +1,87 @@
 package net.fortytwo.iptools;
 
-import java.net.InetAddress;
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
-import java.io.IOException;
+import java.util.Random;
 
 /**
  * User: josh
-* Date: Sep 10, 2010
-* Time: 11:06:08 AM
-*/
+ * Date: Sep 10, 2010
+ * Time: 11:06:08 AM
+ */
 class UdpExperiments {
+    private static final Random RANDOM = new Random();
+
     private final InetAddress receiverAddress;
     private final int receiverPort;
     private final DatagramSocket socket;
 
     public UdpExperiments(final InetAddress receiverAddress,
-                     final int receiverPort) throws SocketException {
+                          final int receiverPort) throws SocketException {
         this.receiverAddress = receiverAddress;
         this.receiverPort = receiverPort;
         this.socket = new DatagramSocket();
     }
 
     public void start() throws IOException {
-        byte[] bytes = "Hello world!".getBytes();
+        send("Hello world!".getBytes());
+        send("What's up, hot stuff?".getBytes());
+    }
 
-        DatagramPacket packet = new DatagramPacket(bytes, bytes.length, receiverAddress, receiverPort);
+    private void send(final byte[] data) throws IOException {
+
+        DatagramPacket packet = new DatagramPacket(data, data.length, receiverAddress, receiverPort);
         socket.send(packet);
     }
-       
+
     public static void main(final String[] args) {
-        int receiverPort = 8006;
 
         try {
-            final UdpReceiver b = new UdpReceiver(receiverPort);
-            new Thread(new Runnable(){
-                public void run() {
-                    try {
-                        b.start();
-                    } catch (IOException e) {
-                        e.printStackTrace(System.err);
-                    }
-                }
-            }).start();
-
-            UdpExperiments t = new UdpExperiments(InetAddress.getLocalHost(), receiverPort);
-            t.start();
-            b.stop();
+            //testLocalReceiver();
+            testRemoveReceiver();
         } catch (Throwable t) {
             t.printStackTrace(System.err);
             System.exit(1);
         }
+    }
+
+    private static void testLocalReceiver() throws Exception {
+        int receiverPort = 8006;
+
+        final UdpReceiver b = new UdpReceiver(receiverPort);
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    b.start();
+                } catch (IOException e) {
+                    e.printStackTrace(System.err);
+                }
+            }
+        }).start();
+
+        UdpExperiments t = new UdpExperiments(InetAddress.getLocalHost(), receiverPort);
+        t.start();
+        b.stop();
+    }
+
+    private static void testRemoveReceiver() throws Exception {
+        InetAddress address = InetAddress.getByName("fortytwo.net");
+        int port = 9990;
+
+        UdpExperiments t = new UdpExperiments(address, port);
+        t.start();
+    }
+
+    ////////////////////////////////////
+
+    private static String randomString(final int length) {
+        byte[] b = new byte[length];
+        for (int i = 0; i < length; i++) {
+            b[i] = (byte) (35 + RANDOM.nextInt(56));
+        }
+        return new String(b);
     }
 }
