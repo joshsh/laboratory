b3174b51baf1e684d8a93021a6fee76d09f5570d
Joshua Shinavier <josh@fortytwo.net>
2010-12-30 23:40:50 +0800

    Trivial style changes to the XMPP MTP, just so you can tell them apart from subsequent, more significant changes.

--- droidspeak/xmpp/src/main/java/net/fortytwo/droidspeak/xmpp/MessageListener.java
+++ droidspeak/xmpp/src/main/java/net/fortytwo/droidspeak/xmpp/MessageListener.java
@@ -1,32 +1,32 @@
 /*****************************************************************
-JADE - Java Agent DEvelopment Framework is a framework to develop
-multi-agent systems in compliance with the FIPA specifications.
-Copyright (C) 2000 CSELT S.p.A.
+ JADE - Java Agent DEvelopment Framework is a framework to develop
+ multi-agent systems in compliance with the FIPA specifications.
+ Copyright (C) 2000 CSELT S.p.A.
 
-The updating of this file to JADE 2.0 has been partially supported by the
-IST-1999-10211 LEAP Project
+ The updating of this file to JADE 2.0 has been partially supported by the
+ IST-1999-10211 LEAP Project
 
-This file refers to parts of the FIPA 99/00 Agent Message Transport
-Implementation Copyright (C) 2000, Laboratoire d'Intelligence
-Artificielle, Ecole Polytechnique Federale de Lausanne
+ This file refers to parts of the FIPA 99/00 Agent Message Transport
+ Implementation Copyright (C) 2000, Laboratoire d'Intelligence
+ Artificielle, Ecole Polytechnique Federale de Lausanne
 
-GNU Lesser General Public License
+ GNU Lesser General Public License
 
-This library is free software; you can redistribute it sand/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation,
-version 2.1 of the License.
+ This library is free software; you can redistribute it sand/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation,
+ version 2.1 of the License.
 
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the
-Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-Boston, MA  02111-1307, USA.
-*****************************************************************/
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the
+ Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ Boston, MA  02111-1307, USA.
+ *****************************************************************/
 
 /**
  * MessageListener.java
@@ -38,12 +38,12 @@ Boston, MA  02111-1307, USA.
  */
 
 
-
 package net.fortytwo.droidspeak.xmpp;
 
 import java.io.StringReader;
 
-import org.jivesoftware.smack.*;
+import org.jivesoftware.smack.PacketListener;
+import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.filter.MessageTypeFilter;
 import org.jivesoftware.smack.filter.PacketFilter;
 import org.jivesoftware.smack.packet.Message;
@@ -55,51 +55,49 @@ import jade.mtp.MTPException;
 import jade.mtp.InChannel.Dispatcher;
 
 public class MessageListener implements PacketListener {
-	private XMPPConnection _con;
-	private Dispatcher _disp;
-	
-	public MessageListener(XMPPConnection con, Dispatcher disp){
-		_con = con;
-		_disp = disp;
-	}
-	
-	public void finalize(){
-		stop();
-	}
-	
-	public void start(){
-		PacketFilter filter = new MessageTypeFilter(Message.Type.normal);
-		_con.addPacketListener(this, filter);		
-	}
-	
-	public void stop(){
-		_con.removePacketListener(this);
-	}
-	
-	public void processPacket(Packet packet){
-		Envelope env;
-		Message msg = (Message)packet;
-		String payload = msg.getBody();
-//		System.out.println(msg.getFrom() + ": " + body);
-		try{
-			//org.apache.xerces.parsers.SAXParser
-			XMLCodec parser = new XMLCodec("org.apache.crimson.parser.XMLReaderImpl");
-			PacketExtension ext = msg.getExtension(FipaEnvelopePacketExtension.ELEMENT_NAME, FipaEnvelopePacketExtension.NAMESPACE);
-			if (ext == null){
-				throw new MTPException("Message do not contains a Envelope!");
-			}
-			FipaEnvelopePacketExtension fipaext = (FipaEnvelopePacketExtension)ext;
+    private XMPPConnection _con;
+    private Dispatcher _disp;
 
-			StringReader sr = new StringReader(fipaext.getEnvelope());
-			env = parser.parse(sr);
-			synchronized (_disp) {
-				_disp.dispatchMessage(env, payload.getBytes());
+    public MessageListener(XMPPConnection con, Dispatcher disp) {
+        _con = con;
+        _disp = disp;
+    }
+
+    public void finalize() {
+        stop();
+    }
+
+    public void start() {
+        PacketFilter filter = new MessageTypeFilter(Message.Type.normal);
+        _con.addPacketListener(this, filter);
+    }
+
+    public void stop() {
+        _con.removePacketListener(this);
+    }
+
+    public void processPacket(Packet packet) {
+        Envelope env;
+        Message msg = (Message) packet;
+        String payload = msg.getBody();
+//		System.out.println(msg.getFrom() + ": " + body);
+        try {
+            //org.apache.xerces.parsers.SAXParser
+            XMLCodec parser = new XMLCodec("org.apache.crimson.parser.XMLReaderImpl");
+            PacketExtension ext = msg.getExtension(FipaEnvelopePacketExtension.ELEMENT_NAME, FipaEnvelopePacketExtension.NAMESPACE);
+            if (ext == null) {
+                throw new MTPException("Message do not contains a Envelope!");
+            }
+            FipaEnvelopePacketExtension fipaext = (FipaEnvelopePacketExtension) ext;
+
+            StringReader sr = new StringReader(fipaext.getEnvelope());
+            env = parser.parse(sr);
+            synchronized (_disp) {
+                _disp.dispatchMessage(env, payload.getBytes());
 //				System.out.println("mensage enviado!");
-			}
-		}
-		catch(MTPException e)
-		{
-		}
-	}
+            }
+        } catch (MTPException e) {
+        }
+    }
 
 }
