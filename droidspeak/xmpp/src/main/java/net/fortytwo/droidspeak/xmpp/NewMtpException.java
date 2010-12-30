4812e195b1f9cddf2a806ed2ee4a1c2f3cc53a28
Joshua Shinavier <josh@fortytwo.net>
2010-12-31 00:35:34 +0800

    Missed this.

---
+++ droidspeak/xmpp/src/main/java/net/fortytwo/droidspeak/xmpp/NewMtpException.java
@@ -0,0 +1,12 @@
+package net.fortytwo.droidspeak.xmpp;
+
+/**
+ * User: josh
+ * Date: 12/31/10
+ * Time: 12:28 AM
+ */
+public class NewMtpException extends Exception {
+    public NewMtpException(final Throwable cause) {
+        super(cause);
+    }
+}
