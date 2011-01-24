2772ac1573eb2bb8317d5ab811030d7e82fdd274
Joshua Shinavier <josh@fortytwo.net>
2011-01-24 16:07:18 +0800

    Demoting 'ajarbeans' to a laboratory project, since I have yet to really do anything with it, and 'ajarbeans' is not a final name.

---
+++ ajarbeans/src/main/java/edu/rpi/tw/ajarbeans/AjarBeans.java
@@ -0,0 +1,16 @@
+package edu.rpi.tw.ajarbeans;
+
+/**
+ * User: josh
+ * Date: Dec 16, 2009
+ * Time: 4:44:29 AM
+ */
+public class AjarBeans {
+    public static void main(final String[] args) throws Exception {
+        
+    }
+
+    private static void doit() {
+        
+    }
+}
