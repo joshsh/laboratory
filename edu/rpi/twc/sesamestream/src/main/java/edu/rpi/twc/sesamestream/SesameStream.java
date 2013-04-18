442ffef06ab55df79a7a73f1f7a222ebcee721f4
Joshua Shinavier <josh@fortytwo.net>
2013-04-18 12:52:47 -0400

    Removed SesameStream project, which has been moved into its own repository

--- edu/rpi/twc/sesamestream/src/main/java/edu/rpi/twc/sesamestream/SesameStream.java
+++
@@ -1,8 +0,0 @@
-package edu.rpi.twc.sesamestream;
-
-/**
- * @author Joshua Shinavier (http://fortytwo.net)
- */
-public class SesameStream {
-    public static final boolean PERFORMANCE_METRICS = true;
-}
