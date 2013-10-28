a2c98320e295ea81de0b2cfe741fe0abddf2b67b
Joshua Shinavier <josh@fortytwo.net>
2013-10-28 16:56:30 -0400

    Support Gremlin as well as Cypher as a benchmarking language for Neo.  Query #1 implemented in Gremlin

--- com/franz/benchmarking/neo4j-benchmarking/src/main/java/com/franz/benchmarking/sp2bench/SP2BenchQueries.java
+++ com/franz/benchmarking/neo4j-benchmarking/src/main/java/com/franz/benchmarking/sp2bench/SP2BenchQueries.java
@@ -1,5 +1,11 @@
 package com.franz.benchmarking.sp2bench;
 
+import com.tinkerpop.blueprints.Vertex;
+import com.tinkerpop.blueprints.impls.neo4j.Neo4jGraph;
+import com.tinkerpop.blueprints.util.wrappers.id.IdGraph;
+import com.tinkerpop.gremlin.groovy.Gremlin;
+import com.tinkerpop.pipes.Pipe;
+import com.tinkerpop.pipes.util.iterators.SingleIterator;
 import org.neo4j.cypher.javacompat.ExecutionEngine;
 import org.neo4j.cypher.javacompat.ExecutionResult;
 import org.neo4j.graphdb.GraphDatabaseService;
@@ -36,23 +42,15 @@ public class SP2BenchQueries {
         System.out.println("iteration time: " + (time3 - time2) + "ms");
     }
 
-    private static void timeQuery(final String query,
-                                  final int iters,
-                                  final int sets,
-                                  final ExecutionEngine engine,
-                                  final List<Long> counts,
-                                  final List<Long> times) {
+    private static void timeCypherQuery(final Query query,
+                                        final int iters,
+                                        final int sets,
+                                        final List<Long> counts,
+                                        final List<Long> times) {
         for (int j = 0; j < sets; j++) {
             long time1 = System.currentTimeMillis();
 
-            long count = 0;
-            for (int i = 0; i < iters; i++) {
-                ExecutionResult result = engine.execute(query);
-                // TODO: restore iteration in AG and Neo test loops
-                for (Map<String, Object> row : result) {
-                    count++;
-                }
-            }
+            long count = query.execute(iters);
 
             long time2 = System.currentTimeMillis();
 
@@ -61,22 +59,21 @@ public class SP2BenchQueries {
         }
     }
 
-    private static void printTimedQuery(final String queryName,
-                                        final String query,
+    private static void printTimedQuery(final Query query,
                                         final int iters,
-                                        final int sets,
-                                        final ExecutionEngine engine) {
+                                        final int sets) {
         List<Long> counts = new LinkedList<Long>();
         List<Long> times = new LinkedList<Long>();
 
-        timeQuery(query, iters, sets, engine, counts, times);
+        timeCypherQuery(query, iters, sets, counts, times);
 
         StringBuilder sb = new StringBuilder();
-        sb.append(queryName);
-        sb.append(".neo4j <- c(");
+        sb.append(query.getName());
+        sb.append(" <- c(");
         boolean first = true;
         for (long time : times) {
-            if (first) first = false; else sb.append(", ");
+            if (first) first = false;
+            else sb.append(", ");
 
             sb.append(time);
         }
@@ -85,11 +82,12 @@ public class SP2BenchQueries {
         System.out.println(sb);
 
         sb = new StringBuilder();
-        sb.append(queryName);
-        sb.append(".neo4j.count <- c(");
+        sb.append(query.getName());
+        sb.append(".count <- c(");
         first = true;
         for (long count : counts) {
-            if (first) first = false; else sb.append(", ");
+            if (first) first = false;
+            else sb.append(", ");
 
             sb.append(count);
         }
@@ -99,20 +97,19 @@ public class SP2BenchQueries {
     }
 
     public static void main(final String[] args) throws Exception {
-        /*
-SELECT ?yr
-WHERE {
-  ?journal rdf:type bench:Journal .
-  ?journal dc:title "Journal 1 (1940)"^^xsd:string .
-  ?journal dcterms:issued ?yr
-}
-         */
 
+        String pathToDatabase = "/tmp/neo-sp2bench";
+
+        new SP2BenchQueries().evaluateCypher(pathToDatabase);
+        //new SP2BenchQueries().evaluateGremlin(pathToDatabase);
+    }
+
+    private void evaluateCypher(final String pathToDatabase) {
         GraphDatabaseFactory factory = new GraphDatabaseFactory();
-        GraphDatabaseService g = factory.newEmbeddedDatabase("/tmp/neo-sp2bench");
+        GraphDatabaseService g = factory.newEmbeddedDatabase(pathToDatabase);
         try {
             // Return the year of publication of Journal 1 (1940).
-            String q1 = "START journal=node(*)\n" +
+            String q1_cypher = "START journal=node(*)\n" +
                     "MATCH journal-[:`rdf:type`]->bj, journal-[:`dc:title`]->title, journal-[:`dcterms:issued`]->year\n" +
                     "WHERE bj.__id = 'bench:Journal' AND title.__id = '\"Journal 1 (1940)\"'\n" +
                     "RETURN year.__id";
@@ -120,7 +117,7 @@ WHERE {
             // Extract all inproceedings with properties
             // dc:creator, bench:booktitle, dc:title, swrc:pages, dcterms:partOf, rdfs:seeAlso, foaf:homepage
             // dcterms:issued, and optionally bench:abstract, including these properties, ordered by year.
-            String q2 = "START inproc=node(*)\n" +
+            String q2_cypher = "START inproc=node(*)\n" +
                     "MATCH inproc-[:`rdf:type`]->bip," +
                     " inproc-[:`dc:creator`]->author," +
                     " inproc-[:`bench:booktitle`]->booktitle," +
@@ -135,26 +132,26 @@ WHERE {
                     "RETURN inproc.__id, author.__id, booktitle.__id, title.__id, proc.__id, ee.__id, page.__id, url.__id, yr.__id, abstract.__id";
 
             // Select all articles with property swrc:pages.
-            String q3a = "START article=node(*)\n" +
+            String q3a_cypher = "START article=node(*)\n" +
                     "MATCH article-[:`rdf:type`]->Art, article-[:`swrc:pages`]->value\n" +
                     "WHERE Art.__id = 'bench:Article'\n" +
                     "RETURN article.__id";
 
             // Select all articles with property swrc:month.
-            String q3b = "START article=node(*)\n" +
+            String q3b_cypher = "START article=node(*)\n" +
                     "MATCH article-[:`rdf:type`]->Art, article-[:`swrc:month`]->value\n" +
                     "WHERE Art.__id = 'bench:Article'\n" +
                     "RETURN article.__id";
 
             // Select all articles with property swrc:isbn.
-            String q3c = "START article=node(*)\n" +
+            String q3c_cypher = "START article=node(*)\n" +
                     "MATCH article-[:`rdf:type`]->Art, article-[:`swrc:isbn`]->value\n" +
                     "WHERE Art.__id = 'bench:Article'\n" +
                     "RETURN article.__id";
 
             // Select all distinct pairs of article author names for authors that have published in the same journal.
             // TODO: very poor performance (perhaps never terminating)
-            String q4 = "START article1=node(*), article2=node(*)\n" +
+            String q4_cypher = "START article1=node(*), article2=node(*)\n" +
                     "MATCH article1-[:`rdf:type`]->Art," +
                     " article2-[:`rdf:type`]->Art," +
                     " article1-[:`dc:creator`]->author1," +
@@ -168,7 +165,7 @@ WHERE {
 
             // Return the names of all persons that occur as author of at least one inproceeding and at least one article.
             // Note: queries q5 and q5a are treated as the same query, as they differ only in FILTER syntax
-            String q5 = "START person=node(*)\n" +
+            String q5_cypher = "START person=node(*)\n" +
                     "MATCH article-[:`rdf:type`]->Art," +
                     " article-[:`dc:creator`]->person," +
                     " inproc-[:`rdf:type`]->Inproc," +
@@ -182,11 +179,120 @@ WHERE {
 
             //showQueryResult(q1, engine);
 
-            printTimedQuery("q3c", q3c, 1, 10, engine);
+            Query q = new CypherQuery("q1.neo", q1_cypher, engine);
+            //Query q = new CypherQuery("q2.neo", q2_cypher, engine);
+            //Query q = new CypherQuery("q3a.neo", q3c_cypher, engine);
+            //Query q = new CypherQuery("q3b.neo", q3c_cypher, engine);
+            //Query q = new CypherQuery("q3c.neo", q3c_cypher, engine);
+            //Query q = new CypherQuery("q4.neo", q4_cypher, engine);
+            //Query q = new CypherQuery("q5.neo", q5_cypher, engine);
 
+            printTimedQuery(q, 1, 10);
         } finally {
             g.shutdown();
         }
+    }
 
+
+    private void evaluateGremlin(final String pathToDatabase) {
+        Neo4jGraph g = new Neo4jGraph(pathToDatabase);
+
+        try {
+            // TODO: necessary?
+            g.getRawGraph().index().getNodeAutoIndexer().startAutoIndexingProperty(IdGraph.ID);
+
+            IdGraph ig = new IdGraph(g, true, false);
+
+            String q1_gremlin_root = "\"Journal 1 (1940)\"";
+            String q1_gremlin = "_().in(\"dc:title\").as(\"journal\").out(\"rdf:type\").filter{it.id.equals(\"bench:Journal\")}.back(\"journal\")";
+
+            Query q = new GremlinQuery("q1.grm", q1_gremlin_root, q1_gremlin, ig);
+
+            printTimedQuery(q, 1, 10);
+        } finally {
+            g.shutdown();
+        }
+    }
+
+
+    private abstract class Query {
+        private final String name;
+
+        protected Query(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public abstract long execute(int iters);
+    }
+
+    private class CypherQuery extends Query {
+        private final String queryString;
+        private final ExecutionEngine engine;
+
+        public CypherQuery(final String name,
+                           final String queryString,
+                           final ExecutionEngine engine) {
+            super(name);
+            this.engine = engine;
+            this.queryString = queryString;
+        }
+
+        public long execute(final int iters) {
+            // Note: if there are multiple iterations, only the last count is used.  All counts shoud be identical.
+            // If for some reason there are no iterations, a count of 0 is used
+            long count = 0;
+
+            for (int i = 0; i < iters; i++) {
+                count = 0;
+                ExecutionResult result = engine.execute(queryString);
+
+                for (Map<String, Object> row : result) {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+
+    private class GremlinQuery extends Query {
+        private final Pipe pipe;
+        private final IdGraph ig;
+        private final String rootId;
+
+        private GremlinQuery(final String name,
+                             final String rootId,
+                             final String queryString,
+                             final IdGraph ig) {
+            super(name);
+            this.ig = ig;
+            this.rootId = rootId;
+
+            pipe = Gremlin.compile(queryString);
+        }
+
+        public long execute(final int iters) {
+            long count = 0;
+
+            for (int i = 0; i < iters; i++) {
+                count = 0;
+
+                Vertex root = ig.getVertex(rootId);
+                if (null == root) {
+                    throw new IllegalStateException("root vertex not found: " + rootId);
+                }
+
+                pipe.setStarts(new SingleIterator<Vertex>(root));
+                for (Object r : pipe) {
+                    count++;
+                }
+            }
+
+            return count;
+        }
     }
 }
