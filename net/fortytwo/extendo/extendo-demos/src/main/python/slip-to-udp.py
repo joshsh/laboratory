537c64cefff7692ca696169495738237a70c65d6
Joshua Shinavier <josh@fortytwo.net>
2014-09-02 15:44:52 -0400

    Refined serial<->UDP bridge utility w.r.t. the SLIP protocol

--- net/fortytwo/extendo/extendo-demos/src/main/python/slip-to-udp.py
+++ net/fortytwo/extendo/extendo-demos/src/main/python/slip-to-udp.py
@@ -1,5 +1,5 @@
-# Python script for serial <--> UDP communication via OSC (Open Sound Control)
-# Extended for OSC by Joshua Shinavier, 2014-08-29
+# Python script for SLIP (Serial Line Internet Protocol) <--> UDP communication
+# By Joshua Shinavier, 2014-08-29
 #     www.fortytwo.net
 # Based on serial_to_udp.py by Alex Olwal, 2012 03 24
 #     www.olwal.com
@@ -19,7 +19,7 @@ def send(msg, ip, port):
     socket(AF_INET,SOCK_DGRAM).sendto(msg, (ip, port))
 
 if ( len(sys.argv) == 1 ):
-        print "Serial<-OSC->UDP utility | Joshua Shinavier, 2014, http://fortytwo.net"
+        print "SLIP<-->UDP utility | Joshua Shinavier, 2014, http://fortytwo.net"
         print "Syntax: " + sys.argv[0] + " serial_port udp_ip(= 127.0.0.1) udp_in_port(= 5000) udp_out_port(=5001)"
         print "Example: " + sys.argv[0] + " COM20 127.0.0.1 5000 5001"
         quit()
@@ -50,7 +50,7 @@ udp_out_port = int(udp_out_port)
 
 sport = serial.Serial( serial_port, baud_rate, timeout=1 )
 
-def receive(ip):
+def receive():
     serial_out_buffer = array('c', ' '*1500)
     in_sock = socket(AF_INET, SOCK_DGRAM)
     in_sock.bind(("", udp_in_port))
@@ -62,28 +62,37 @@ def receive(ip):
             sport.write(serial_out_buffer[i])
         sport.write(chr(192))
 
-thread = Thread(target = receive, args = (udp_ip, ))
+thread = Thread(target = receive)
 thread.start()
 
-while (1):
-	b = sport.read()
-	if (0 == len(b)):
-		continue
-	elif (192 != ord(b)):
-		print "expected byte 192, found " + str(ord(b))
-		continue
-	else:
-		serial_in_buffer[0] = b
-		i = 1			
-		while (True):
-			b = sport.read()
-			serial_in_buffer[i] = b 
-			i = i + 1
-			if (192 == ord(b)):
-				st = "".join(map(chr, serial_in_buffer[1:i-1]))
-				if (printing):
-				    print "serial(" + serial_port + ")->UDP(" + str(udp_out_port) + "): " + str(len(st)) + " bytes"
-				send(st, udp_ip, udp_out_port)
-				break
+# Loop while threads are running.
+try :
+    # skip to the end of the current datagram
+    while (1):
+        b = sport.read()
+        if (0 == len(b)):
+            continue
+        elif (192 == ord(b)):
+            break
 
-sport.close()
+    i = 0
+    while (True):
+        b = sport.read()
+        if (0 == len(b)):
+            continue
+        if (192 == ord(b)):
+            if (i > 0):
+                st = "".join(map(chr, serial_in_buffer[0:i]))
+                if (printing):
+                    print "serial(" + serial_port + ")->UDP(" + str(udp_out_port) + "): " + str(len(st)) + " bytes"
+                send(st, udp_ip, udp_out_port)
+                i = 0
+        else:
+            serial_in_buffer[i] = b
+            i = i + 1
+
+except KeyboardInterrupt :
+    print "closing..."
+    sport.close()
+    thread.join()
+    print "done"
