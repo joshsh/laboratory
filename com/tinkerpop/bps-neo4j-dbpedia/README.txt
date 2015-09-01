eadf9aab0083218f1048372b7c31d38d7f7f7bb1
Joshua Shinavier <josh@fortytwo.net>
2015-09-01 07:44:57 -0700

    Replaced WordNet example with generic SailLoader example

--- com/tinkerpop/bps-neo4j-dbpedia/README.txt
+++
@@ -1,1 +0,0 @@
-time java -Xms2048m -Xmx2048m -cp target/bps-neo4j-dbpedia-0.1-full.jar net.fortytwo.BlueprintsSailNeo4jDBpediaLoader 2>&1 | tee out.txt
---
+++ com/tinkerpop/bps-neo4j-dbpedia/src/main/java/net/fortytwo/SailLoaderExample.java
@@ -0,0 +1,27 @@
+package net.fortytwo;
+
+import com.tinkerpop.blueprints.impls.neo4j.Neo4jGraph;
+import com.tinkerpop.blueprints.oupls.sail.GraphSail;
+import com.tinkerpop.blueprints.oupls.sail.SailLoader;
+import org.openrdf.sail.Sail;
+
+import java.io.File;
+
+/**
+ * @author Joshua Shinavier (http://fortytwo.net)
+ */
+public class SailLoaderExample {
+    public static void main(final String[] args) throws Exception {
+        Neo4jGraph g = new Neo4jGraph("/tmp/wordnet/neo4j");
+
+        Sail sail = new GraphSail(g);
+        sail.initialize();
+        try {
+            SailLoader loader = new SailLoader(sail);
+            loader.setVerbose(true);
+            loader.load(new File("/tmp/wordnet/rdf"));
+        } finally {
+            sail.shutDown();
+        }
+    }
+}
